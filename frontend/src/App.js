2e0cb808f0853d023af9d3acc89bd4a7e066aca3
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-03-12 22:54:47 +0300

    frontend - Qty Selected & Add To Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import { Container } from "react-bootstrap"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-import HomeScreen from "./screens/HomeScreen"
-import ProductScreen from "./screens/ProductScreen"
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" Component={HomeScreen} exact />
             <Route path="/product/:id" Component={ProductScreen} />
+            <Route path="/cart/:id?" Component={CartScreen} />
           </Routes>
         </Container>
       </main>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,8 @@
 // import axios from 'axios'
-// import { useState, useEffect } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
@@ -11,19 +10,19 @@ import Message from '../components/Message'
 import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = () => {
-  const params = useParams()
-
   // const [product, setProduct] = useState([])
-
   // useEffect(() => {
   //   async function fetchProduct() {
   //     const { data } = await axios.get(`/api/products/${params.id}`)
   //     setProduct(data)
   //   }
-
   //   fetchProduct()
   // }, [params.id])
 
+  const [qty, setQty] = useState(1)
+
+  const params = useParams()
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const productDetails = useSelector((state) => state.productDetails)
   const { loading, error, product } = productDetails
@@ -31,6 +30,10 @@ const ProductScreen = () => {
   useEffect(() => {
     dispatch(listProductDetails(params.id))
   }, [dispatch, params])
+
+  const addToCartHandler = () => {
+    navigate(`/cart/${params.id}?qty=${qty}`)
+  }
 
   return (
     <div>
@@ -87,8 +90,30 @@ const ProductScreen = () => {
                   </Row>
                 </ListGroup.Item>
 
+                {product.count_in_stock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(event) => setQty(event.target.value)}
+                        >
+                          {[...Array(product.count_in_stock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className="btn-block"
                     disabled={product.count_in_stock === 0}
                     type="button"
