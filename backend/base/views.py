e3f3c52f11b940aa53c8fcd10bcda51bf8f2cd6d
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-03-16 17:38:46 +0300

    backend - JWT Customization.

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,27 @@
+from typing import Any
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .products import products
 from .models import Product
 from .serializers import ProductSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: dict[str, Any]) -> dict[str, str]:
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
