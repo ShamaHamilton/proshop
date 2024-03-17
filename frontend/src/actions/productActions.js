6949ed8c975d9ce4aba1fc2a51ba11ac9dc16f44
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-03-17 14:12:11 +0300

    frontend - User Login Reducer & Action.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,7 +20,7 @@ export const listProducts = () => async (dispatch) => {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        error.response && error.response.data.message ? error.response.message.data : error.message,
+        error.response && error.response.data.detail ? error.response.data.detail : error.message,
     })
   }
 }
@@ -36,7 +36,7 @@ export const listProductDetails = (id) => async (dispatch) => {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
       payload:
-        error.response && error.response.data.message ? error.response.message.data : error.message,
+        error.response && error.response.data.detail ? error.response.data.detail : error.message,
     })
   }
 }
