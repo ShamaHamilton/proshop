d12e9dc1d003131276773c898c9e184035789375
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-03-19 23:08:46 +0300

    fix: frontend - User Details Bug Fix.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -11,6 +11,7 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
@@ -52,6 +53,7 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_DETAILS_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -144,12 +146,12 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     const { data } = await axios.put(`/api/users/profile/update/`, user, config)
 
     dispatch({
-      type: USER_UPDATE_PROFILE_REQUEST,
+      type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
     })
 
     dispatch({
-      type: USER_LOGIN_REQUEST,
+      type: USER_LOGIN_SUCCESS,
       payload: data,
     })
 
