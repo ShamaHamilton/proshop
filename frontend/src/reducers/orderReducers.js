bb9cde1832bfa1bca5a48b70d2dac68babf7bf3d
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-03-24 09:12:45 +0300

    frontend - Order Screen.

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -42,16 +42,19 @@ export const orderDetailsReducer = (
         ...state,
         loading: true,
       }
+
     case ORDER_DETAILS_SUCCESS:
       return {
         loading: false,
         order: action.payload,
       }
+
     case ORDER_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,
       }
+
     default:
       return state
   }
